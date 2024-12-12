336cd6f091953364467abed917ee44dc9e36868e
Baldr27 <g.jara08@ufromail.cl>
2024-12-12 16:35:32 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+from fasthtml.common import *
+
+
+
+app, rt = fast_app()
+
+@app.get("/")
+def home():
+    page = Html(
+        Head(
+            Title('Test'),
+            picolink,
+        ),
+        Body(
+            header,
+            Main(
+                H1('Welcome to the Main Page'),
+                P('Content goes here...'),
+                cls='container',
+            ),
+            style,
+        ),
+        data_theme="light"
+    )
+    return page
+
+@app.get("/miembros")
+def miembros():
+    page = Html(
+        Head(
+            Title('Miembros'),
+            picolink,
+        ),
+        Body(
+            header,
+            Main(
+                H1('Welcome to the Members Page'),
+                P('Hello members!'),
+                cls='container',
+            ),
+            style,
+        ),
+        data_theme="light"
+    )
+    return page
+
+@app.get("/publicaciones")
+def publicaciones():
+    page = Html(
+        Head(
+            Title('Publicaciones'),
+            picolink,
+        ),
+        Body(
+            header,
+            Main(
+                H1('Welcome to the Publications Page'),
+                P('Publications here'),
+                cls='container',
+            ),
+            style,
+        ),
+        data_theme="light"
+    )
+    return page
+
+picolink = Link(rel="stylesheet", href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css")
+header = Header(
+    Nav(
+        A('LOGO', href='/', cls='logo'),
+        Div(
+            A('Miembros', href='/miembros'),
+            A('Publicaciones', href='/publicaciones'),
+            cls='tabs'
+        ),
+    ),
+    cls='banner'
+    )
+style = Style(
+    ".content {margin: 0 auto; max-width: 1200px; background-color: white;}" +
+    ".banner {height: 50px; background-color: #332c2f; color: white; padding: 0 20px; margin: 0 auto; width: 100%;}" +
+    ".logo {font-size: 1.5em; font-weight: bold;}" +
+    ".tabs {display: flex; gap: 40px; justify-content: center; align-items: center; margin: 0 auto;}"
+    )
+
+serve()
